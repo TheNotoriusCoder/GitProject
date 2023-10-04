e372be494eb2112bb1de1bdfd8ce2f0888af95b0
Nicolai Jurea <nicolaijurea@Nicolais-MBP.hsd1.il.comcast.net>
2023-10-04 12:40:42 -0500

    deleted 2 lines

--- src/test/java/GitIntroduction.java
+++ src/test/java/GitIntroduction.java
@@ -1,7 +1,6 @@
 public class GitIntroduction {
     ////leaving comment
-    ////
-    ////comment number 2
+
     public static void main(String[] args) {
         System.out.println("fuck you");
     }

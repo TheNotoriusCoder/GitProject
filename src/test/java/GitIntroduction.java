ade2c893778037907cce30eb4d1cca66741ffe1f
Nicolai Jurea <nicolaijurea@Nicolais-MBP.hsd1.il.comcast.net>
2023-10-05 10:29:06 -0500

    added one line

--- src/test/java/GitIntroduction.java
+++ src/test/java/GitIntroduction.java
@@ -3,5 +3,8 @@ public class GitIntroduction {
 
     public static void main(String[] args) {
         System.out.println("fuck you");
+        System.out.println("huitaget status" +
+                "");
     }
+
 }
